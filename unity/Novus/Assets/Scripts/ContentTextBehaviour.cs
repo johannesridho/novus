20272107af6367204ec9c0ed10672860363491f0
sujono91 <sujonochen91@gmail.com>
2018-05-01 22:44:20 -0700

    Implement Text Changing

---
+++ unity/Novus/Assets/Scripts/ContentTextBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContentTextBehaviour : MonoBehaviour {
+	// Use this for initialization
+	private TextMesh contentText;
+
+	void Start () {
+		contentText = GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			contentText.text = "Welcome to F8!";
+		}
+	}
+}
