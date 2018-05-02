44fdf22772695c6e23398632e49b7ed894f74636
Sandy Cahya <sandy.cahya.23@gmail.com>
2018-05-02 03:15:12 -0700

    firebase

---
+++ unity/Novus/Assets/Scripts/firebase.cs
@@ -0,0 +1,14 @@
+﻿using Firebase;
+using Firebase.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firebase : MonoBehaviour
+{
+    void Start()
+    {
+        FirebaseApp app = FirebaseApp.DefaultInstance;
+        app.SetEditorDatabaseUrl("https://novus-9e2c2.firebaseio.com/");
+    }
+}
