8e2032e780cbb9ec3f1159e34003ef0b0b354a64
Sandy Cahya <sandy.cahya.23@gmail.com>
2018-05-01 23:07:21 -0700

    animation

--- unity/Novus/Assets/Scripts/ContentTextBehaviour.cs
+++ unity/Novus/Assets/Scripts/ContentTextBehaviour.cs
@@ -15,7 +15,20 @@ public class ContentTextBehaviour : MonoBehaviour {
 	void Update () {
 		if (Input.GetKey(KeyCode.RightArrow))
 		{
-			contentText.text = "Welcome to F8!";
-		}
+            Color newColor = new Color(1, 1, 1, 0);
+            transform.GetComponent<Renderer>().material.color = newColor;
+            contentText.text = "Welcome to F8!";
+            StartCoroutine(FadeTo(1.0f, 1.0f));
+        }
 	}
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        float alpha = transform.GetComponent<Renderer>().material.color.a;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha, aValue, t));
+            transform.GetComponent<Renderer>().material.color = newColor;
+            yield return null;
+        }
+    }
 }
---
+++ unity/Novus/Assets/script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Sprite backSprite = Resources.Load<Sprite>("resources/books.png");
+        GameObject obj = new GameObject("book");
+        SpriteRenderer rend = obj.AddComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+        obj.transform.position = Vector3.zero;
+        rend.sprite = backSprite;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
