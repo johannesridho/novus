2c6e6084c7d7d3143d69f5aae677baf073d3e3ae
johannesridho <johanridho@gmail.com>
2018-05-02 15:26:57 +0700

    add fb login

--- unity/Novus/Assets/Scripts/ContentTextBehaviour.cs
+++ unity/Novus/Assets/Scripts/ContentTextBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Facebook.Unity;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,6 +11,18 @@ public class ContentTextBehaviour : MonoBehaviour {
 	private static string[] CONTENTS = {"Connect with friends and the\nworld around you on Facebook.", 
 		"Welcome to F8!", "Had a great experience at F8!", "Greatest hackathon so far", "Cool!"};
 
+	// Awake function from Unity's MonoBehavior
+	void Awake ()
+	{
+		if (!FB.IsInitialized) {
+			// Initialize the Facebook SDK
+			FB.Init(InitCallback, OnHideUnity);
+		} else {
+			// Already initialized, signal an app activation App Event
+			FB.ActivateApp();
+		}
+	}
+
 	void Start () {
 		contentText = GetComponent<TextMesh>();
 		counter = 0;
@@ -19,6 +32,9 @@ public class ContentTextBehaviour : MonoBehaviour {
 	void Update () {
 		if (Input.GetKeyUp(KeyCode.RightArrow) && counter < CONTENTS.Length - 1)
 		{
+			var perms = new List<string>(){"public_profile", "email"};
+			FB.LogInWithReadPermissions(perms, AuthCallback);
+
 			counter++;
 			Color newColor = new Color(1, 1, 1, 0);
 			transform.GetComponent<Renderer>().material.color = newColor;
@@ -46,4 +62,42 @@ public class ContentTextBehaviour : MonoBehaviour {
             yield return null;
         }
     }
+
+	private void InitCallback ()
+	{
+		if (FB.IsInitialized) {
+			// Signal an app activation App Event
+			FB.ActivateApp();
+			// Continue with Facebook SDK
+			// ...
+		} else {
+			Debug.Log("Failed to Initialize the Facebook SDK");
+		}
+	}
+
+	private void OnHideUnity (bool isGameShown)
+	{
+		if (!isGameShown) {
+			// Pause the game - we will need to hide
+			Time.timeScale = 0;
+		} else {
+			// Resume the game - we're getting focus again
+			Time.timeScale = 1;
+		}
+	}
+
+	private void AuthCallback (ILoginResult result) {
+		if (FB.IsLoggedIn) {
+			// AccessToken class will have session details
+			var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+			// Print current access token's User ID
+			Debug.Log(aToken.UserId);
+			// Print current access token's granted permissions
+			foreach (string perm in aToken.Permissions) {
+				Debug.Log(perm);
+			}
+		} else {
+			Debug.Log("User cancelled login");
+		}
+	}
 }
