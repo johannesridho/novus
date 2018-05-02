cb78f08de2772be00fff80b1c660a1b629e7690a
johannesridho <johanridho@gmail.com>
2018-05-02 13:51:30 +0700

    update text contents

--- unity/Novus/Assets/Scripts/ContentTextBehaviour.cs
+++ unity/Novus/Assets/Scripts/ContentTextBehaviour.cs
@@ -6,21 +6,36 @@ using UnityEngine.UI;
 public class ContentTextBehaviour : MonoBehaviour {
 	// Use this for initialization
 	private TextMesh contentText;
+	private int counter;
+	private static string[] CONTENTS = {"Connect with friends and the\nworld around you on Facebook.", 
+		"Welcome to F8!", "Had a great experience at F8!", "Greatest hackathon so far", "Cool!"};
 
 	void Start () {
 		contentText = GetComponent<TextMesh>();
+		counter = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.RightArrow))
+		if (Input.GetKeyUp(KeyCode.RightArrow) && counter < CONTENTS.Length - 1)
 		{
-            Color newColor = new Color(1, 1, 1, 0);
-            transform.GetComponent<Renderer>().material.color = newColor;
-            contentText.text = "Welcome to F8!";
-            StartCoroutine(FadeTo(1.0f, 1.0f));
-        }
+			counter++;
+			Color newColor = new Color(1, 1, 1, 0);
+			transform.GetComponent<Renderer>().material.color = newColor;
+			contentText.text = CONTENTS[counter];
+			StartCoroutine(FadeTo(1.0f, 2.0f));
+		}
+
+		if (Input.GetKeyUp(KeyCode.LeftArrow) && counter > 0)
+		{
+			counter--;
+			Color newColor = new Color(1, 1, 1, 0);
+			transform.GetComponent<Renderer>().material.color = newColor;
+			contentText.text = CONTENTS[counter];
+			StartCoroutine(FadeTo(1.0f, 1.0f));
+		}
 	}
+
     IEnumerator FadeTo(float aValue, float aTime)
     {
         float alpha = transform.GetComponent<Renderer>().material.color.a;
